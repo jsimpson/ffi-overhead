82f0719ba287f06a5b23b6013470352741002285
Jonathan Simpson <jsimpson.github@gmail.com>
2018-05-27 11:09:12 -0700

    quick and dirty ruby test

---
+++ hello.rb
@@ -0,0 +1,37 @@
+require 'ffi'
+
+module Hello
+  class C
+    extend FFI::Library
+
+    ffi_lib 'newplus/libnewplus.so'
+
+    functions = [
+      [:current_timestamp, [],           :long_long],
+      [:plus,              [:int, :int], :int],
+      [:plusone,           [:int],       :int]
+    ]
+
+    functions.each do |function|
+      begin
+        attach_function *function
+      rescue Object => e
+        puts "Could not attach #{function}, #{e.message}"
+      end
+    end
+  end
+end
+
+def run(count)
+  start = Hello::C.current_timestamp
+
+  x = 0
+  while x < count do
+    x = Hello::C.plusone(x)
+  end
+
+  put "#{Hello::C.current_timestamp - start}"
+end
+
+run(500000000)
+
